da99e754992ac59af5b09768fb03461d251ed510
yutyann5213 <yutyanntyenn5213@gmail.com>
2023-09-05 16:43:10 +0900

    ボタンの処理

---
+++ teamAfusion/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChangeScene: MonoBehaviour
+{
+
+    float Count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnButton();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+
+    void OnButton()
+    {       
+        Debug.Log("‰Ÿ‚³‚ê‚½");      
+    }
+ 
+}
