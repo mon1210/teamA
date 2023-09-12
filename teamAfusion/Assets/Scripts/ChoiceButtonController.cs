269f2f58f669a6ceb302499f47664c14e2ce7787
yutyann5213 <yutyanntyenn5213@gmail.com>
2023-09-12 11:07:38 +0900

    update ボス表示シーンに飛ぶように変更

--- teamAfusion/Assets/Scripts/ChoiceButtonController.cs
+++ teamAfusion/Assets/Scripts/ChoiceButtonController.cs
@@ -6,20 +6,16 @@ using UnityEngine.UI;
 
 
 public class ChoiceButtonController : MonoBehaviour
-{
-    //選択ボタンの取得
-    [SerializeField] GameObject[] Button = new GameObject[1];
+{   
     //選択状態取得
     [SerializeField] bool[] isSelect = new bool[1];
-    //テキスト取得
-    [SerializeField] Text text;
 
     //スタートボタンの関数
     public void OnChoiceButton()
     {
         isSelect[0] = !isSelect[0];
 
-        SceneManager.LoadScene("BattleScene1-1");
+        SceneManager.LoadScene("BoosDisplayScene");
     }
 
 
