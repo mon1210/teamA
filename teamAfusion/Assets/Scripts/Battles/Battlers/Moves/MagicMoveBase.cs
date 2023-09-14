e3be5411fc3e91e5589b54e24ea6d819ae19a52d
mon1210 <himejijin@i.softbank.jp>
2023-09-14 22:25:35 +0900

    add MagicわざのBaseスクリプトを作成

---
+++ teamAfusion/Assets/Scripts/Battles/Battlers/Moves/MagicMoveBase.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//MoveBase(Name)を継承した「まほう」技の基礎データ
+[CreateAssetMenu]
+public class MagicMoveBase : MoveBase
+{
+    //
+    [SerializeField] int power;
+    //
+    [SerializeField] int magicPoint;
+    //外部で取得できるように
+    public int MagicPoint { get => magicPoint; }
+    public int Power { get => power; }
+
+    //関数のオーバーライド
+    public override string RunMoveResult(BattleUnit sourcerUnit, BattleUnit targetUnit)
+    {
+        //int型でmagicPointを受け取る
+        sourcerUnit.Battler.Magic(magicPoint);
+        //int型で受け取ったダメージをセット
+        int damage = targetUnit.Battler.TakeDamage(power, sourcerUnit.Battler);
+        //ダメージを与えた・受けたログを返す
+        return $"{sourcerUnit.Battler.Base.Name}の「{Name}」！\n{targetUnit.Battler.Base.Name}に{damage}のダメージ！";
+    }
+}
