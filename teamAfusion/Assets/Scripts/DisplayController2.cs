f53a514f6341e52097240c4d7710df39c194ab51
yutyann5213 <yutyanntyenn5213@gmail.com>
2023-09-15 12:05:07 +0900

    add スクリプトの追加

---
+++ teamAfusion/Assets/Scripts/DisplayController2.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DisplayController2: MonoBehaviour
+{
+    //ï\é¶Ç…ä÷Ç∑ÇÈèàóù
+    public void OnBanlceDisplayButton1()
+    {
+        SceneManager.LoadScene("B.FirstBattleScene");
+    }
+    public void OnBanlceDisplayButtonn2()
+    {
+        SceneManager.LoadScene("B.SecondBattleScene");
+    }
+    public void OnBanlceDisplayButton3()
+    {
+        SceneManager.LoadScene("B.LastBattleScene");
+    }
+}
